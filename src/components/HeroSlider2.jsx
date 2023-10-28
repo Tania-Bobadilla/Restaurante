c223db0ae438a2af0d2932cea6537ffcc381a4f7
tania.b <carolinaortz1@gmail.com>
2023-10-28 20:48:40 -0300

    se cambia directorio imagenes

--- src/components/HeroSlider2.jsx
+++ src/components/HeroSlider2.jsx
@@ -5,16 +5,16 @@ const HeroSlider2 = () => {
   return (       
         <Carousel interval={null} className="text-center container-md">
               <Carousel.Item>
-                  <img src="../../images/res1.jpg"  />
+                  <img src="assets/res1.jpg"  />
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src="../../images/food..jpg"/>
+                  <img src="assets/food..jpg"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src="../../images/res2.webp"/>
+                  <img src="assets/images/res2.webp"/>
               </Carousel.Item>
               <Carousel.Item>
-                  <img src="../../images/samosas.jpg"/>
+                  <img src="assets/images/samosas.jpg"/>
               </Carousel.Item>
         </Carousel>
   )
